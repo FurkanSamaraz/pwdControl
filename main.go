61027dca23634fbfa5437acaf6a2c33d65715e17
Furkan Samaraz <92402372+FurkanSamaraz@users.noreply.github.com>
2022-02-03 04:19:51 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
-package passwordd
+package CheckPWD
 
 import "regexp"
 
 func CheckPWD(pwd string) bool {
-	match, _ := regexp.MatchString("((?=.*/d)(?=.*[A-Z])(?=.*\W).{8,8}) ",pwd)
+	match, _ := regexp.MatchString("((?=.*/d)(?=.*[A-Z])(?=.*/W).{8,8}) ", pwd)
 	return match
 }
